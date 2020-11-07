252fb3689fbbca9651ee4287d3164180b55ca92f
Millad <Milladayoubi@gmail.com>
2020-11-06 23:49:05 -0500

    Base Application W/ Firebase

---
+++ app/src/main/java/com/example/pyroapplication/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.pyroapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.os.Bundle;
+
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private  int count = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(getApplicationContext(),"Connected To FireBase DataBase",Toast.LENGTH_SHORT).show();
+        FloatingActionButton fab = findViewById(R.id.fab);
+        final TextView textBox1 = findViewById(R.id.txt1);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (count <= 10 )
+                Snackbar.make(view, "Bitch You Ain't Shit", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+                else
+                    Snackbar.make(view, "Look who is counting faster then Nevada!!!", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+
+                count += 1;
+
+                textBox1.setText("" + count);
+
+
+
+
+
+            }
+
+
+        });
+    }
+    }
