3343e8c923be322372ec16f6915b8b6de68cf57d
Millad <Milladayoubi@gmail.com>
2020-11-07 20:33:58 -0500

    Base Application W/ Firebase

--- app/src/main/java/com/example/pyroapplication/MainActivity.java
+++ app/src/main/java/com/example/pyroapplication/MainActivity.java
@@ -8,55 +8,22 @@ import android.os.Bundle;
 
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 
 public class MainActivity extends AppCompatActivity {
-    Button workoutButton;
-    private  int count = 0;
+     Button workoutButton = findViewById(R.id.login);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(getApplicationContext(),"Connected To FireBase DataBase",Toast.LENGTH_SHORT).show();
-        FloatingActionButton fab = findViewById(R.id.fab);
-
-        workoutButton = findViewById(R.id.login);
-
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                if (count <= 10 )
-                Snackbar.make(view, "Hello There! ", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-
-                else
-                    Snackbar.make(view, "Look who is counting faster then Nevada!!!", Snackbar.LENGTH_LONG)
-                            .setAction("Action", null).show();
-
-                count += 1;
 
 
-
-
-
-
-
-            }
-
-
-        });
-
-            workoutButton.setOnClickListener(new View.OnClickListener(){
+        workoutButton.setOnClickListener(new View.OnClickListener(){
 
                 @Override
-                public void onClick(View view) {
-                    Intent intent = new Intent(MainActivity.this , workoutActivity.class);
+                public void onClick(View v) {
+                    Intent intent = new Intent(MainActivity.this, workoutPage.class);
                     startActivity(intent);
                 }
             }) ;
