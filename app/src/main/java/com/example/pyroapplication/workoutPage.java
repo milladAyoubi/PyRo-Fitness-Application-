90b6c9446cb57032896d5989e2af53797318fb68
Millad <Milladayoubi@gmail.com>
2020-11-11 22:54:12 -0500

    Calender Implementation

--- app/src/main/java/com/example/pyroapplication/workoutPage.java
+++ app/src/main/java/com/example/pyroapplication/workoutPage.java
@@ -4,26 +4,35 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.Toast;
 
 public class workoutPage extends AppCompatActivity {
-    CalendarView calanderView;
+    CalendarView calenderView;
+
+
+    int calories;
+    int duration;
+    String type;
+    Button workoutEnter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout);
 
-        calanderView = (CalendarView) findViewById(R.id.workoutDate);
+        calenderView =  findViewById(R.id.workoutDate);
+        workoutEnter = findViewById(R.id.enterButton);
 
-        calanderView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+        calenderView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
                 String date = (dayOfMonth) + "/" + month + "/" + year;
                 Toast.makeText(workoutPage.this, date, Toast.LENGTH_SHORT).show();
             }
         });
+
 
 
     }
